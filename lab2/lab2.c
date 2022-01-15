2bda37f05740dddc4a82886501cbe8a90ba8697e
guilhermeTerrell <95145860+guilhermeTerrell@users.noreply.github.com>
2022-01-15 16:41:33 -0300

    Add files via upload
    Lab 2 -> final version

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,49 +1,80 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-
-//Operações: Adicionar -> 0 e Consultar -> 1
-
-int vectorSize(int *vetor){
-    int i=0;
-    for(i=0; vetor[i]!='\0'; i++){
+void adiciona(int *ptrVetorIdade, int *ptrVetorCasos, int tamanhoNecessario, int *counter){
+    //Inicio da funcao
+    int idade, casos, i=0;
+    scanf("%d %d", &casos, &idade);
+    //Verificar se já existe essa idade no vetor de idade
+    for(i=0; i<tamanhoNecessario; i++){
+        if(ptrVetorIdade[i]==idade){
+            //Idade já existe
+            printf("idade já existe\n");
+            ptrVetorCasos[i]=ptrVetorCasos[i]+casos;
+            printf("%d %d\n", ptrVetorCasos[i], ptrVetorIdade[i]);
+            break;
+        }
+        else if(i==tamanhoNecessario-1){
+            //Idade nova
+            //Armazenar idade e casos na próxima posição
+            printf("idade nova %d\n", i);
+            ptrVetorIdade[*counter]=idade;
+            ptrVetorCasos[*counter]=casos;
+            printf("%d %d\n", ptrVetorCasos[*counter], ptrVetorIdade[*counter]);
+            (*counter)++;
+        }
     }
-    return i;
 }
 
-//Função que verifica se determinada idade X já existe no vetor de idade
-void adiciona(int *vetorIdade, int *vetorCasos){
-    int i, size, jaExiste=0;
-    int idade, casos;
-    size=vectorSize(vetorIdade);
-    scanf("%d %d", vetorCasos, vetorIdade);
-}
-
-void imprime_ponteiro(int n, int *ptr1, int *ptr2){
-    int i=0;
-    for(i=0; i<n; i++){
-        printf("%d %d\n", ptr1[i], ptr2[i]);
+void consulta(int *ptrVetorIdade, int *ptrVetorCasos, int *counter, int *totalDeCasos){
+    int idadeMin, idadeMax, total, i=0;
+    printf("Digite o intervalo de idade a ser consultado\n");
+    scanf("%d %d", &idadeMin, &idadeMax);
+    for(i=0; i<(*counter); i++){
+        if((ptrVetorIdade[i]>=idadeMin) && (ptrVetorIdade[i]<=idadeMax)){
+            printf("counter: %d\n", (*counter));
+            printf("casos: %d\n", ptrVetorCasos[i]);
+            (*totalDeCasos)=(*totalDeCasos)+ptrVetorCasos[i];
+        }
     }
 }
+
 
 int main(){
-    int i, n, operacao=0;
+    int i, n, operacao, counter=0;
+    int totalDeCasos=0;
     int tamanhoInicial=5;
-    int *idade[tamanhoInicial];
-    int *casosCovid[tamanhoInicial];
-    printf("Insira a quantidade de operações\n");
+    //Inicialização dos vetores
+    int tamanhoNecessario=tamanhoInicial;
+    int vetorIdade=malloc(tamanhoNecessario*sizeof(int));
+    int vetorCasos=malloc(tamanhoNecessario*sizeof(int));
+    //Inicialização dos ponteiros
+    int *ptrVetorIdade=vetorIdade;
+    int *ptrVetorCasos=vetorCasos;
+    printf("Digite a quantidade de operacoes\n");
     scanf("%d", &n);
     for(i=0; i<n; i++){
-        printf("insira a operacao a ser realizada\n");
+        printf("Digite a operacao\n");
         scanf("%d", &operacao);
         if(operacao==0){
             //adiciona
-            adiciona(idade, casosCovid);
+            adiciona(ptrVetorIdade, ptrVetorCasos, tamanhoNecessario, &counter);
+            if(counter==tamanhoNecessario){
+                //vetor cheio
+                printf("Alocando mais memoria\n");
+                tamanhoNecessario=2*(tamanhoNecessario);
+                realloc(ptrVetorIdade, tamanhoNecessario);
+                realloc(ptrVetorCasos, tamanhoNecessario);
+            }
         }
         if(operacao==1){
             //consulta
+            consulta(ptrVetorIdade, ptrVetorCasos, &counter, &totalDeCasos);
+            printf("Total de casos: %d\n", totalDeCasos);
         }
+        totalDeCasos=0;
     }
-    imprime_ponteiro(tamanhoInicial, casosCovid, idade);
+    free(vetorCasos);
+    free(vetorIdade);
     return 0;
 }
+
