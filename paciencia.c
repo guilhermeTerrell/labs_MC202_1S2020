64ca7346b617d8b4e2d850aea926357063c943c0
Guilherme Terrell <guilherme.terrell1994@gmail.com>
2023-01-16 20:45:55 -0300

    adicionanado as funções inicializaPilha(), imprimePilha(), destroiPilha, verificaCartaAberta(), verificaPilhaDescarte(), moveCarta(), abreCartasDaPilha(), abreCartasDeJogo()

--- paciencia.c
+++ paciencia.c
@@ -2,6 +2,7 @@
     MC202
     Lab 1:      Paciência
     Objetivo:   Implementar em linguagem C o jogo de cartas chamado de "paciência"
+    Link:       https://www.ic.unicamp.br/~francesquini/mc202/files/lab01.pdf
     Método:     As pilhas de jogo serão implementadas utilizando lista ligada,
                 |                 | 
                 |    elemento_n  <-  top
@@ -13,11 +14,11 @@
                 onde top é um ponteiro que aponta sempre para o último elemento da pilha
                 
                 Os elementos da pilha serão structs que conterão:
-                    1-) Valor e naipe da carta;
+                    1-) Valor da carta;
                     2-) Naipe da carta
                     3-) A cor da carta
                     4-) Uma flag que indica se a carta está aberta ou virada
-                    5-) Um ponteiro para o próximo nó
+                    5-) Um ponteiro para a próxima carta
 */
 
 /*********************          Includes            *********************************/
@@ -64,7 +65,7 @@ typedef enum estadoDaCarta{
         Se naipe == "O" ou naipe == "C" -> carta.cor = VERMELHA
         Se naipe == "P" ou naipe == "E" -> carta.cor = PRETA 
 
-    Além disso a struct possui como membro um ponteiro que deve apontar para o endereço da proóxima carta,
+    Além disso a struct possui como membro um ponteiro que deve apontar para o endereço da próxima carta,
     possibilitando assim a criação de uma pilha através de lista ligada
 */
 typedef struct carta{
@@ -89,7 +90,9 @@ pilha_cartas top_J5 = NULL; /*ponteiro que aponta para o topo da pilha de jogo J
 pilha_cartas top_J6 = NULL; /*ponteiro que aponta para o topo da pilha de jogo J6*/
 pilha_cartas top_J7 = NULL; /*ponteiro que aponta para o topo da pilha de jogo J7*/
 
-pilha_cartas estoque = NULL; /*ponteiro que aponta para o topo da pilha de estoque*/
+pilha_cartas pilha_estoque = NULL; /*ponteiro que aponta para o topo da pilha de estoque*/
+
+pilha_cartas pilha_descarte = NULL; /*ponteiro que aponta para o topo da pilha de descarte*/
 
 pilha_cartas saida_ouros = NULL; /*ponteiro que aponta para o topo da pilha de saída do naipe ouros*/
 pilha_cartas saida_paus = NULL; /*ponteiro que aponta para o topo da pilha de saída do naipe paus*/
@@ -109,14 +112,20 @@ pilha_cartas * ptr_to_top_J5 = &top_J5; /*ponteiro para o ponteiro top_J5*/
 pilha_cartas * ptr_to_top_J6 = &top_J6; /*ponteiro para o ponteiro top_J6*/
 pilha_cartas * ptr_to_top_J7 = &top_J7; /*ponteiro para o ponteiro top_J7*/
 
-
 /******************             Protótipos das funções  *****************************/
 void push(pilha_cartas *);
 void pop(pilha_cartas *);
 void criaNo(void);
 unsigned char pilhaVazia(pilha_cartas *);
 void peek(pilha_cartas *);
-
+void inicializaPilha(pilha_cartas *);
+void imprimePilha(pilha_cartas *);
+void destroiPilha(pilha_cartas *);
+estadoDaCarta verificaCartaAberta(pilha_cartas *);
+void verificaPilhaDescarte(void);
+void moveCarta(pilha_cartas *, pilha_cartas *);
+void abreCartasDaPilha(pilha_cartas *);
+void abreCartasDeJogo();
 
 /*****************               Definição das funções     ***************************/
 /*
@@ -148,7 +157,7 @@ void push(pilha_cartas * ptrTOptr){
     if(*ptrTOptr == NULL){
         /*se o ponteiro apontar para NULL siginifica que a pilha está vazia*/
         /*inserir primeriro nó*/
-        (*ptrTOptr) = temp; /*armazenar o espaço de memória alocado no membro carta_baralho*/
+        *ptrTOptr = temp; /*armazenar o espaço de memória alocado no membro carta_baralho*/
         (*ptrTOptr) -> ptr_prox_carta = NULL; /*primeiro no da pilha deve sempre apontar para NULL*/
     }
     else{
@@ -178,8 +187,8 @@ void pop(pilha_cartas * ptrTOptr){
 
 /*
     Função:     peek
-    Descrição:  Consultar o dado armazenado no último nó da pilha
-    Argumentos: Ponteiro que aponta para a pilha em questão
+    Descrição:  Consultar o dado armazenado em uma carta da pilha
+    Argumentos: Ponteiro para o ponteiro que aponta para a carta de interesse (geralmente o top)
 */
 void peek(pilha_cartas * ptrTOptr){
     printf("valor: %d\n", (*ptrTOptr) -> valor);
@@ -199,27 +208,147 @@ void peek(pilha_cartas * ptrTOptr){
     printf("ptr_prox: %d\n", (*ptrTOptr) -> ptr_prox_carta);
 }
 
+/*
+    Função:     inicializaPilha
+    Descrição:  Atribui valores a pilha recém criada
+    Argumentos: Ponteiro para o ponteiro que aponta para o top da pilha em questão
+*/
+void inicializaPilha(pilha_cartas * ptrTOptr){
+
+    unsigned int i = 1;
+
+    for(i = 1; i <= 12; i++){
+
+        push(ptrTOptr); /*inserir uma carta na pilha*/
+
+        (*ptrTOptr) -> valor = i; /*valor da carta*/
+
+        /*naipe da carta*/
+        ((*ptrTOptr) -> naipe)[0] = 'O';
+
+        /*a cor da carta é preenchida por software*/
+        if((((*ptrTOptr) -> naipe)[0] == 'O') || (((*ptrTOptr) -> naipe)[0] == 'C')){
+            (*ptrTOptr) -> cor = VERMELHA;
+        }
+        if((((*ptrTOptr) -> naipe)[0] == 'P') || (((*ptrTOptr) -> naipe)[0] == 'E')){
+            (*ptrTOptr) -> cor = PRETA;
+        }
+        
+        (*ptrTOptr) -> abertaOuVirada = CARTA_VIRADA;
+    }
+}
+
+/*
+    Função:     imprimePilha
+    Descrição:  Percorrer todas as cartas de uma pilha de cartas imprimindo as informções de
+                valor da carta, naipe, se está aberta ou virada, etc
+    Argumentos: Ponteiro para o ponteiro que aponta para o top da pilha em questão
+*/
+void imprimePilha(pilha_cartas * ptrTOptr){
+    /*loop será executado enquanto houver mais de uma carta*/
+    while((*ptrTOptr) -> ptr_prox_carta != NULL){
+        peek(ptrTOptr);
+        printf("-----------------------------\n");
+        (*ptrTOptr) = (*ptrTOptr) -> ptr_prox_carta; /*apontar carta_atual para a próxima carta da pilha*/
+    }
+    /*quando (*ptrTOptr) -> ptr_prox_carta = NULL significa que só restou a primeira carta da pilha*/
+    /*imprimir a primeira carta da pilha*/
+    peek(ptrTOptr);
+}
+
+/*
+    Função:     destroiPilha
+    Descrição:  Libera todo o espaço de memória que foi alocado para a pilha
+    Argumentos: Ponteiro para o ponteiro que aponta para o top da pilha em questão
+*/
+void destroiPilha(pilha_cartas * ptrTOptr){
+    /*(*ptrTOptr) -> ptr_prox_carta != NULL há mais de uma carta na pilha*/
+    while((*ptrTOptr) -> ptr_prox_carta != NULL){
+        pop(ptrTOptr); /*libera o espaço de memória da carta atual*/
+        (*ptrTOptr) = (*ptrTOptr) -> ptr_prox_carta; /*apontar carta_atual para a próxima carta da pilha*/
+    }
+    /*quando (*ptr_to_top_J1) -> ptr_prox_carta == NULL significa que só há uma carta na pilha*/
+    pop(ptrTOptr);
+}
+
+
+/*
+    Função:     verificaCartaAberta
+    Descrição:  Verificar se a carta apontada pelo ponteiro está aberta. Somente cartas abertas podem ser movidas
+                entre as pilhas de jogo (VER REGRA 1!)
+    Argumentos: Ponteiro para o ponteiro que aponta para o top da pilha em questão
+    Saída:      Retorna CARTA_ABERTA (1) se a carta estiver aberta ou CARTA_FECHADA(0) se a carta estiver virada.
+*/
+estadoDaCarta verificaCartaAberta(pilha_cartas * ptrTOptr){
+    return (*ptrTOptr) -> abertaOuVirada;
+}
+
+/*
+    Função:     verificaPilhaDescarte
+    Descrição:  Verificar se a pilha de descarte está vazia. Se sim é preciso mover a primeira carta da pilha
+                de estoque para a pilha de descarte (VER REGRA 2!)
+*/
+void verificaPilhaDescarte(){
+    if(pilha_descarte == NULL){
+        /*pilha de descarte está vazia*/
+        pilha_descarte = pilha_estoque; /*apontar o ponteiro de pilha descarte para a primeira carta da pilha de estoque*/
+        pilha_estoque = pilha_estoque -> ptr_prox_carta; /*atualizar o ponteiro da pilha de estoque para a próxima carta*/
+        pilha_descarte -> ptr_prox_carta = NULL; /*apontar ptr_prox_carta pois essa é a unica carta na pilha de estoque*/
+    }
+}
+
+/*
+    Função: moveCarta
+    Descrição:  Função que move cartas de uma pilha para outra, respeitando as regras estabelecidas
+    Argumentos: pilha de origem, pilha de destino
+    Saída:      printar na tela a pilha de origem, pilha de destino e carta sendo movimentada
+*/
+void moveCarta(pilha_cartas * ptrTOptr_origem, pilha_cartas * ptrTOptr_destino){
+    if((*ptrTOptr_origem) -> ptr_prox_carta == NULL){
+        /*só existe essa carta na pilha*/
+        (*ptrTOptr_origem) -> ptr_prox_carta = *ptrTOptr_destino; /*apontar o ptr_prox para o topo da outra pilha*/
+        *ptrTOptr_destino = *ptrTOptr_origem; /*atualizar o ponteiro do topo da pilha de destino para o endereço da nova carta*/
+        *ptrTOptr_origem = NULL; /*pilha de origem que só tinha uma carta fica vazia após move-la para outr pilha*/
+    }
+}
+
+/*
+    Função:     abreCartasDaPilha
+    Descrição:  Função que abre as cartas de jogo de uma pilha em específico, i.e. cartas que estão no topo da pilha de jogo.
+                caso o membro "abertaOuVirada" da struct carta seja igual a CARTA_VIRADA significa que a primeira carta da pilha
+                ainda está virada, e portanto deve ter seu valor alterado para CARTA_ABERTA (VER REGRA 3!)
+    Argumentos: Ponteiro para o ponteiro que aponta para o top da pilha em questão
+*/
+void abreCartasDaPilha(pilha_cartas * ptrTOptr){
+    /*deve haver ao menos uma carta na pilha*/
+    if(((*ptrTOptr) != NULL) && ((*ptrTOptr) -> abertaOuVirada == CARTA_VIRADA)) (*ptrTOptr) -> abertaOuVirada = CARTA_ABERTA;
+}
+
+/*
+    Função: abreCartasDeJogo
+    Descrição:  Função que abre sequencialmente todas as cartas de jogo, i.e. cartas que estão no topo das pilhas de jogo (VER REGRA 3!). 
+*/
+void abreCartasDeJogo(){
+    abreCartasDaPilha(ptr_to_top_J1);
+    abreCartasDaPilha(ptr_to_top_J2);
+    abreCartasDaPilha(ptr_to_top_J3);
+    abreCartasDaPilha(ptr_to_top_J4);
+    abreCartasDaPilha(ptr_to_top_J5);
+    abreCartasDaPilha(ptr_to_top_J6);
+    abreCartasDaPilha(ptr_to_top_J7);
+}
+
+
+/***********************         Main           ***********************************/
 int main(){
 
-    push(ptr_to_top_J1); /*inserir uma carta na pilha*/
+    inicializaPilha(ptr_to_top_J3);
 
-    top_J1 -> valor = 10; /*valor da carta*/
+    abreCartasDeJogo(ptr_to_top_J3);
 
-    /*naipe da carta*/
-    (top_J1 -> naipe)[0] = 'O';
-    if(((top_J1 -> naipe)[0] == 'O') || ((top_J1 -> naipe)[0] == 'C')){
-        top_J1 -> cor = VERMELHA;
-    }
-    if(((top_J1 -> naipe)[0] == 'P') || ((top_J1 -> naipe)[0] == 'E')){
-        top_J1 -> cor = PRETA;
-    }
+    imprimePilha(ptr_to_top_J3);
     
-    top_J1 -> abertaOuVirada = CARTA_VIRADA;
-
-    peek(ptr_to_top_J1);
-    
-    pop(ptr_to_top_J1);
+    destroiPilha(ptr_to_top_J3);
     
     return 0;
-
 }
